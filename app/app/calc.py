108065fea129921d0c1f09c9702b613969b07871
Henri-Rene Kuningas <henri.kuningas@finbite.eu>
2022-06-04 08:33:58 +0300

    Added core and tests for db

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculate function
+"""
+
+
+def add(x, y):
+    """Add x and y and return result"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract x from y and return value"""
+    return y - x
