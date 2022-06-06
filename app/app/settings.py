38bb60b4c6d8206ed1a93b2d0873c4b3222c2370
Henri-Rene Kuningas <henri.kuningas@finbite.eu>
2022-06-06 13:46:00 +0300

    Added superuser functionality, model

--- app/app/settings.py
+++ app/app/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+# sup-u contact@dealitgr.... sY8
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -127,3 +127,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
