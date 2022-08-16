bb5a7deebdfdc20812b171494fc9d72a59f57386
henrirene <henrirene@gmail.com>
2022-08-16 08:43:14 +0300

    Recipe Tags listing added, lesson92

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -66,3 +66,13 @@ class ModelTests(TestCase):
         )
         
         self.assertEqual(str(recipe), recipe.title)
+    
+    def test_create_tag(self):
+        """Test creating a tag is successful."""
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'testpass123',
+        )
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+        
+        self.assertEqual(str(tag), tag.name)
