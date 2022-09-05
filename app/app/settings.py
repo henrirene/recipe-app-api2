46a9f755ed86d0da19e135af332def1fed66945d
henrirene <henrirene@gmail.com>
2022-09-05 21:49:22 +0300

    deployment conf issue

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,12 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x(5cqoovg6hc7z(!3bkh_380$kh-@*r4z%mmq!v#vz(rub1$76'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')#'django-insecure-x(5cqoovg6hc7z(!3bkh_380$kh-@*r4z%mmq!v#vz(rub1$76'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
