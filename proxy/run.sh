5722dbac02d922d1b2d6947a0e79d531367782d4
henrirene <henrirene@gmail.com>
2022-09-26 19:21:48 +0300

    local proxy settings issues

--- proxy/run.sh
+++ proxy/run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-
-set -e
-
-envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
-nginx -g 'daemon off;'
+\n
+set -e\n
+\n
+envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf\n
+nginx -g 'daemon off;'\n
+\n
