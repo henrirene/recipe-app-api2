b4bc086fcf33c6f60214a2f0af9ef19b2f24ee17
henrirene <henrirene@gmail.com>
2022-10-08 11:12:54 +0300

    working solution in local

--- proxy/run.sh
+++ proxy/run.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-\n
-set -e\n
-\n
-envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf\n
-nginx -g 'daemon off;'\n
-\n
+
+set -e
+
+envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
+nginx -g 'daemon off;'
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,9 @@
-#!/bin/sh
+#!/bin/sh\n
 \n
-set -e\n
-\n
-python manage.py wait_for_db\n
-python manage.py collectstatic --noinput\n
-python manage.py migrate\n
-\n
-uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi\n
-\n
+set -e
+
+python manage.py wait_for_db
+python manage.py collectstatic --noinput
+python manage.py migrate
+
+uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
